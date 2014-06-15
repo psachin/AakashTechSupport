f8f6311f8770a301d5d4958ffdac52ceebd5c8aa
Shubham Utwal <shubhamutwal@gmail.com>
2014-06-16 02:09:05 +0530

    CSS modified on some pages
    1. Questions Page
    2. Tags Page

--- questions/urls.py
+++ questions/urls.py
@@ -8,5 +8,8 @@ urlpatterns = patterns(
     #url(r'^$', views.main, name='main'),
 
     url(r'^()$', views.all_questions_view, name='question'),
+    url(r'^tag/$', views.tag, name='tag'),
+    url(r'^tag_search/$', views.tag_search, name='tag_search'),
+
 
 )
--- questions/views.py
+++ questions/views.py
@@ -3,6 +3,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response, render, get_object_or_404
 from aakashuser.models import Post, Reply
+from taggit.models import Tag
 from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import Count
 from django.contrib.auth.decorators import login_required
@@ -46,4 +47,49 @@ def all_questions_view(request, url):
             'posts': posts,
         }
 
+    return render_to_response('questions/all_questions.html', context_dict, context)
+
+
+def tag(request):
+    context = RequestContext(request)
+    tags = Tag.objects.all()
+
+    for i in tags:
+        tag.count = len(Post.objects.filter(tags=i))
+
+    context_dict = {
+        'tags': tags
+    }
+
+    return render_to_response('forum/tags.html', context_dict, context)
+
+
+def linktag(request, id):
+    context = RequestContext(request)
+    new_tag = Tag.objects.get(pk=id)
+    posts = Post.objects.filter(tags=new_tag).order_by('-post_date')
+    posts1 = Post.objects.filter(tags=new_tag).order_by('-post_views')
+    context_dict = {
+        'posts': posts,
+        'mytag': new_tag,
+        'posts1': posts1
+    }
+    return render_to_response('questions/all_questions.html', context_dict, context)
+
+
+def tag_search(request):
+    context = RequestContext(request)
+    mytag = request.POST.get('search_text')
+    mytag = mytag.upper()
+    try:
+        new_tag = Tag.objects.get(name=mytag)
+        posts = Post.objects.filter(tags=new_tag).order_by('-post_date')
+        posts1 = Post.objects.filter(tags=new_tag).order_by('-post_views')
+        context_dict = {
+            'posts': posts,
+            'mytag': new_tag,
+            'posts1': posts1
+        }
+    except Tag.DoesNotExist:
+        context_dict = {}
     return render_to_response('questions/all_questions.html', context_dict, context)
